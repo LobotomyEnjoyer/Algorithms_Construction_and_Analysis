97ab8c9359f8b64b68db09d1a78efe21f27f36f9
[blank] <76683471+LobotomyEnjoyer@users.noreply.github.com>
2025-11-15 17:22:08 +0400

    edited t1

--- t1.py
+++ t1.py
@@ -1,16 +1,32 @@
-# МНОГОЧЛЕН
+# МНОГОЧЛЕН (ДИОФАНТОВОЕ УРАВНЕНИЕ)
 
-# МЕТОД ПЕРЕБОРА (ЦИКЛОМ)
 
-START = 0
-STOP = 31
 
-count = 0
-for a in range(START, STOP):
-    for b in range(START, STOP):
-        for c in range(START, STOP):
-            for d in range(START, STOP):
-                if( a + 2*b + 3*c + 4*d == 30):
-                    count += 1
+choice = int(input("Выберите метод решения:" \
+"1) МЕТОД ПЕРЕБОРА" \
+"2) ГЕНЕТИЧЕСКИЙ АЛГОРИТМ" \
+"0) ВЫХОД"))
 
-print(f"in [{START}, {STOP}) total answers found: {count}")
+
+
+
+match choice:
+    case 1: # МЕТОД ПЕРЕБОРА (ЦИКЛОМ)
+     START = 0
+     STOP = 31
+
+     count = 0
+     for a in range(START, STOP):
+         for b in range(START, STOP):
+             for c in range(START, STOP):
+                 for d in range(START, STOP):
+                     if(a + 2*b + 3*c + 4*d == 30):
+                         count += 1
+
+     print(f"in [{START}, {STOP}) total answers found: {count}")
+    case 2: #  ГЕНЕТИЧЕСКИЙ АЛГОРИТМ
+     pass
+    case 0:
+     print("Выход")
+    case _:
+     print("Выход")
